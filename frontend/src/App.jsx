761d7576dbefbe83ac1c35e0d9a867d8675deeb6
Balayya@12 <nivasreddy6868@gmail.com>
2026-03-24 08:04:24 +0530

    Initial commit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 
-const API_URL = "https://fastapi-example-hlnn.onrender.com";
+const API_URL = import.meta.env.VITE_API_URL;
 
 function App() {
   const [form, setForm] = useState({
